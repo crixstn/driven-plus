78f4d92d5a6140f17c7fd524e967657a6b3d8f6b
crixstn <crixstn@gmail.com>
2022-12-26 18:50:50 -0300

    feat: added Login Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useContext } from "react";
+import UserContext from "./contexts/UserContext";
 
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import SubscriptionsPage from "./pages/SubscriptionsPage/SubscriptionsPage";
 import PlanInfoPage from "./pages/SubscriptionsPage/PlanInfoPage";
 import HomePage from "./pages/HomePage/HomePage"
+import { useState } from "react";
 
 
 export default function App() {
+
+  const [userData, setUserData] = useState(JSON.parse(localStorage.getItem("userData")));
+  const [token, setToken] = useState("");
+  const [name, setName] = useState("");
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage/>}/>
-        <Route path="/sign-up" element={<SignUpPage/>}/>
-        <Route path="/home" element={<HomePage/>}/>
-        <Route path="/subscriptions" element={<SubscriptionsPage/>}/>
-        <Route path="/subscriptions/id" element={<PlanInfoPage/>}/>
-      </Routes>
+      <UserContext.Provider value={{
+        token, setToken, userData, setUserData, name, setName 
+      }}>
+        <Routes>
+          <Route path="/" element={<LoginPage/>}/>
+          <Route path="/sign-up" element={<SignUpPage/>}/>
+          <Route path="/home" element={<HomePage/>}/>
+          <Route path="/subscriptions" element={<SubscriptionsPage/>}/>
+          <Route path="/subscriptions/id" element={<PlanInfoPage/>}/>
+        </Routes>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,22 +1,83 @@
 import {LoginLayout, Logo} from "./styled"
 import LinkStyle from "../../shared/LinkStyle"
 import logo from "../../assets/logo.png"
+import Url from "../../constants/Url"
 
-import { useState } from "react"
+import UserContext from "../../contexts/UserContext"
+import { useContext, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
 export default function LoginPage(){
+    const navigate = useNavigate()
+
+    const {setUserData} = useContext(UserContext)
+    const {setToken} = useContext(UserContext)
+    const {setName} = useContext(UserContext)
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function Login(e){
+        e.preventDefault()
+        const body = {
+            email,
+            password
+        }
+
+        const promisse = axios.post(`${Url}/auth/login`, body)
+
+        promisse.then(res => {
+            const data = res.data
+
+            const userInfos = {
+                email: data.email,
+                id: data.id,
+                name: data.name,
+                membership: data.membership,
+                token: data.token
+            }
+
+            localStorage.setItem("userData", JSON.stringify(userInfos))
+            setUserData(JSON.parse(localStorage.getItem("userData")))
+
+            setToken(res.data.token)
+            setName(res.data.name)
+
+            const membership = res.data.membership;
+
+            if(membership === null){
+                navigate("/subscriptions")
+            }else{
+                navigate("/home")
+            }
+
+        })
+
+        promisse.catch( err => {
+            alert(err.response.data.message)
+        })
+
+    }
+
     return(
         <LoginLayout>
             <Logo src={logo} />
-            <form>
+            <form onSubmit={Login}>
                 <input
                     type="email"
                     placeholder="E-mail"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
                 />
 
                 <input
                     type="password"
                     placeholder="Senha"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
                 />
 
                 <button>ENTRAR</button>
